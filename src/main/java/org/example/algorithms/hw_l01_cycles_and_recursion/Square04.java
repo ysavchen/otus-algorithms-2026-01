291655a596c904c6f83c038a6183d42803fb5e33
Yuriy Savchenko <ysavchen@outlook.com>
2026-01-31 19:00:24 +0300

    Update squares

--- src/main/java/org/example/algorithms/hw_l01_cycles_and_recursion/Square04.java
+++ src/main/java/org/example/algorithms/hw_l01_cycles_and_recursion/Square04.java
@@ -2,13 +2,13 @@ package org.example.algorithms.hw_l01_cycles_and_recursion;
 
 import static java.lang.System.lineSeparator;
 
-public class Square03 {
+public class Square04 {
 
     public static void main(String[] args) {
         int n = 25;
         for (int y = 0; y < n; y++) {
             for (int x = 0; x < n; x++) {
-                var result = (x == 24 - y) ? "#" : ".";
+                var result = (x + y < 30) ? "#" : ".";
                 System.out.print(result);
             }
             System.out.print(lineSeparator());
